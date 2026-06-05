ae6f897f1b20dacb140aececa907a65e3b5ab1f8
CuentaGit1234 <feryjbrghthjhcfry@gmail.com>
2026-06-05 10:28:04 +0000

    3

--- backend.py
+++ backend.py
@@ -63,9 +63,6 @@ enumerate(prediction)}
         'probs': probabilities
     })
 
-if __name__== '__main__': 
-	app.run(debug=False, port=5000, host='0.0.0.0')
-
 if __name__ == '__main__':
     import os
     port = int(os.getenv('PORT', 5000))
